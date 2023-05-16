c2e7a04f94ad6aabe164160a57009af231d550ae
VinayCh0wdary <vinay.ch@mindsetconsulting.com>
2023-05-16 19:23:46 +0530

    Updates

---
+++ precache-manifest.ec8051658eac3459d30a51af7211d83d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a4a62a1053226e3bc4e759fea260736",
+    "url": "/my-react-portfolio/index.html"
+  },
+  {
+    "revision": "9e5196c438595993ddc6",
+    "url": "/my-react-portfolio/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "9f72abe4f86257abdcab",
+    "url": "/my-react-portfolio/static/js/2.70802be7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-react-portfolio/static/js/2.70802be7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e5196c438595993ddc6",
+    "url": "/my-react-portfolio/static/js/main.f13ff4d0.chunk.js"
+  },
+  {
+    "revision": "35f9e58eb32da499e28b",
+    "url": "/my-react-portfolio/static/js/runtime-main.880269f4.js"
+  },
+  {
+    "revision": "339aa27e9767c0a412604e95e7c36ce4",
+    "url": "/my-react-portfolio/static/media/about.339aa27e.png"
+  },
+  {
+    "revision": "bcb3f2a650b6dadf092a1102ce1523c9",
+    "url": "/my-react-portfolio/static/media/img-3.bcb3f2a6.jpg"
+  },
+  {
+    "revision": "bce76e1949031a2862923f3addafc5d2",
+    "url": "/my-react-portfolio/static/media/s1.bce76e19.jpg"
+  }
+]);
